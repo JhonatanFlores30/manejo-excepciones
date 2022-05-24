03b8449346f911cdf38fcb7aa541f466c566df53
Jhonatan Flores <105456332+JhonatanFlores30@users.noreply.github.com>
2022-05-24 11:40:27 -0500

    V1.cuadradoNum
    carga de un numero al cuadrado

---
+++ manejo-excepciones/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manejo_excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            try
+            {
+                Console.Write("Ingresa el número que quieres elevar: ");
+                numero = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("El cuadrado del numero es: " + Math.Pow(numero, 2));
+            }
+            catch(FormatException x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            catch(OverflowException x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
